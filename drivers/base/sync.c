e97a96421ed2936fb8d527124dfab8657f640728
kabata1975 <kabata1975@yahoo.es>
2015-12-16 19:46:35 +0100

    Fix sync: fix timeout = 0 wait behavior

--- drivers/base/sync.c
+++ drivers/base/sync.c
@@ -477,7 +477,7 @@ int sync_fence_wait(struct sync_fence *fence, long timeout)
 
 	if (fence->status < 0) {
 		pr_info("fence error %d on [%p]\n", fence->status, fence);
-		sync_dump();
+		
 		return fence->status;
 	}
 
@@ -485,7 +485,6 @@ int sync_fence_wait(struct sync_fence *fence, long timeout)
 		if (timeout > 0) {
 			pr_info("fence timeout on [%p] after %dms\n", fence,
 				jiffies_to_msecs(timeout));
-			sync_dump();
 		}
 		return -ETIME;
 	}
