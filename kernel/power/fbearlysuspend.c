b0d8e0cdd448f8af1ebd3cabb32483460c06aab1
kabata1975 <kabata1975@yahoo.es>
2016-05-31 17:11:17 +0200

    fix Early Suspend Delay v1.0a - SysFS interface

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -39,16 +39,10 @@ static void stop_drawing_early_suspend(struct early_suspend *h)
 	int ret;
 	unsigned long irq_flags;
 
-<<<<<<< HEAD
-	/* FIXME: earlysuspend breaks androids CRT-off animation
-	* Sleep a little bit to get it played properly */
-	msleep(fbearlysuspend_delay); 
-=======
 #ifdef CONFIG_EARLY_SUSPEND_DELAY
 	if (early_suspend_delay != EARLY_SUSPEND_DISABLED)
 		msleep(early_suspend_delay); /* Allow for a little pause so CRT-off animation can be played (initial idea credits to codeworkx) */
 #endif
->>>>>>> 617476a... Early Suspend Delay v1.0a - SysFS interface
 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
 	fb_state = FB_STATE_REQUEST_STOP_DRAWING;
